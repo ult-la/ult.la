d46843321e0a9b2bd332ca0d360148e750dd9781
nera1 <nera4936@gmail.com>
2026-03-30 23:50:50 +0900

    feat: apply pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import MainLayout from "@/layouts/MainLayout";
 import { ThemeProvider } from "@/contexts/ThemeContext";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body style={{ fontFamily: "'Pretendard Variable', sans-serif" }}>
         <ThemeProvider>
           <MainLayout>{children}</MainLayout>
         </ThemeProvider>
