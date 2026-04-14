faaf6b7f08a29a1c71e3a2c19b7f3b1439f0e724
nera1 <nera4936@gmail.com>
2026-04-14 22:14:35 +0900

    feat: restore right sidebar in main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,19 +1,44 @@
 "use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import Navbar from "@/components/layout/Navbar";
+import RightSidebar from "@/components/sidebar/RightSidebar";
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const pathname = usePathname();
+  const hideRightSidebar = [
+    "/events",
+    "/event",
+    "/friends",
+    "/favorites",
+    "/marketplace",
+    "/dating",
+    "/settings",
+    "/groups",
+    "/messages",
+    "/marketplace/item",
+    "/saved",
+  ].includes(pathname);
+
   return (
     <div className="min-h-screen bg-palette-bg">
       <Navbar />
 
       <div className="pt-16 pb-8">
-        <div className="max-w-7xl mx-auto">
-          <div className="py-4">{children}</div>
+        <div className="max-w-7xl mx-auto flex flex-col lg:flex-row">
+          {/* Main Content */}
+          <div className="flex-1 py-4">{children}</div>
+
+          {/* Right Sidebar - Hidden on mobile and specific routes */}
+          {!hideRightSidebar && (
+            <div className="hidden lg:block lg:flex-shrink-0">
+              <RightSidebar />
+            </div>
+          )}
         </div>
       </div>
     </div>
